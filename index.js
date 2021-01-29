44c4a59b13659b3cabc7f67088694aa3a1828c88
rinelson456 <nel15031@byui.edu>
2021-01-29 15:30:55 -0700

    prove04

--- index.js
+++ index.js
@@ -34,23 +34,29 @@ app.use(errorController.get404);
 
 mongoose
     .connect(
-        'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false'
+        MONGODB_URL, options
     )
     .then(result => {
-        User.findOne().then(user => {
-            if (!user) {
-                const user = new User({
-                    name: 'Max',
-                    email: 'max@test.com',
-                    cart: {
-                        items: []
-                    }
-                });
-                user.save();
-            }
-        });
-        app.listen(5000);
+        // This should be your user handling code implement following the course videos
+        app.listen(PORT);
     })
     .catch(err => {
         console.log(err);
     });
+
+const cors = require('cors') // Place this with other requires (like 'path' and 'express')
+const corsOptions = {
+    origin: "https://<your_app_name>.herokuapp.com/",
+    optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+const options = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    family: 4
+};
+
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://<username>:<username>@cse341cluster-3dwlw.mongodb.net/test?retryWrites=true&w=majority";
